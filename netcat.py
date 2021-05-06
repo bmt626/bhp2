3aa4abe4eb7aa05b273f6a7d31f0078b7fa77fce
Brandon Taylor <brandon@Brandons-MacBook-Air.local>
2021-05-06 18:13:50 -0400

    adding tcp and udp client, tcp server, and starting netcat.py

---
+++ netcat.py
@@ -0,0 +1,24 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.slipt(cmd),stderr=subprocess.STDOUT)
+    return output.decode()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='BHP Net Tool', formatter_class=argparse.RawDescriptionHelpFormatter=textwrap.dedent('''Example: netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell 
+    netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
+    netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
+    echo 'ABC' | ./netcat.py -t 192.168.0.108 -p 135 # echo text to server port 135
+    netcat.py -t 192.168.0.108 -p 5555 # connect to server '''))
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
